9e741694f32650885d0904ff5ad0b64194f7bd94
redlegblackarm <hrishikesh123s@gmail.com>
2019-03-26 18:23:48 +0530

    test folder added

---
+++ test/input0.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include "abc.h"
+
+//Hello World!!
+
+int global = 100;
+
+/*skjdkalsjdklsjadd
+sal;kd;laskd;laks
+salkdl;askdsdlsdk*/
+
+void fun1(){ }
+
+void fun2(int a, int b, int c){
+	
+	int ad=1;
+}
+
+struct test{
+	//int i,j,k;
+	string fun(){a=1;}
+	int i,j,k;
+};
+
+class test{
+	public:
+	  int a,b,c;
+	private:
+	  void fun(){}
+	protected:
+	  int fun2(){
+		  int a=2;
+	  }
+};
+
+int main(){
+	int a;
+	a=3,b=2;
+	int d=2, c=2;
+	double array[5][5][5];
+	cout << "Hello";
+	cin >> b;
+	
+	1>2;
+	1&2;
+	1&&2;
+	{
+		1 == 2;
+	}
+	{
+		3 < 4;
+	}
+	{
+		return 0;
+	}
+	{
+		return 1;
+	}
+	
+	for (int i=0; i<10 ; i++){
+		cout<<i;
+	}
+	
+	while(i<10){
+		cout<<i;
+		--i;
+	}
+	
+	if(i>a)
+		cout<<a;
+	if(i>b){
+		cout<<b;
+	}
+	if(i>c){
+		cout<<c;
+	}
+	else{
+		cout<<d;
+	}
+	
+	return 0;
+	
+}
---
+++ test/input1.cpp
@@ -0,0 +1,5 @@
+int main()
+{
+	int a=5;
+	int b=3,c=1;
+}
