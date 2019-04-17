6e58875358df47c783dfd7989507331c445cf72a
redlegblackarm <hrishikesh123s@gmail.com>
2019-04-17 21:20:59 +0530

    astgen.h added

--- astgen.cpp
+++ astgen.cpp
@@ -8,39 +8,7 @@
 */
 
 #include<bits/stdc++.h>
-
-// for representing all branches
-struct ast_node{
-	int node_type;
-	std::string symbol;
-	// for binary tree, use left & right
-	/*
-	ast_node *left;
-	ast_node *right;
-	*/
-	// for generic tree use, vector of ast_node
-	std::vector<ast_node *> children;
-};
-
-// syntax tree
-struct ast_body{
-	const char *head_symbol = "<body>";
-	/*
-	std::string head_symbol;
-	*/
-	std::vector<ast_node*> children;
-};
-
-// stack to maintain leafs and nodes for AST
-std::deque<ast_node*> S_ast;
-
-// root element for syntax tree v1
-ast_node *ast_root = new ast_node;
-// c-root is not used anywhere right now
-ast_node *c_root = ast_root;
-
-// root element for syntax tree v2
-ast_body *syn_root = new ast_body;
+#include "astgen.h"
 
 // make a node for AST
 ast_node *make_ast_node(std::string op, struct ast_node *left_leaf, struct ast_node *right_leaf){
@@ -113,6 +81,7 @@ ast_node *create_branch(ast_node *i_root, ast_node *branch){
 	return i_root;
 }
 
+// central node creation of exp evaluation
 ast_node *central_node_creation(std::string op){
 	ast_node *left_leaf = S_ast.front();
 	S_ast.pop_front();
@@ -123,6 +92,7 @@ ast_node *central_node_creation(std::string op){
 	return central_node;
 }
 
+// alternate central node creation for exp evaluation
 ast_node *central_node_creation_exp(std::string op){
 	ast_node *right_leaf = S_ast.front();
 	S_ast.pop_front();
@@ -133,6 +103,7 @@ ast_node *central_node_creation_exp(std::string op){
 	return central_node;
 }
 
+// declaration and expression
 ast_node *central_node_creation_declaration(std::string op){
 	ast_node *right_leaf = S_ast.front();
 	S_ast.pop_front();
@@ -168,7 +139,6 @@ void printPostorder(struct ast_node *node){
 */
 
 // branch-wise traversal for syntax tree
-// pre-order of each node
 void LevelOrderTraversalAST(){
 	std::cout << "SYNTAX TREE (BRANCH-WISE - LevelOrder)" << std::endl;
 	std::cout << syn_root->head_symbol << std::endl;
---
+++ astgen.h
@@ -0,0 +1,74 @@
+// for representing all branches
+struct ast_node{
+	int node_type;
+	std::string symbol;
+	// for binary tree, use left & right
+	/*
+	ast_node *left;
+	ast_node *right;
+	*/
+	// for generic tree use, vector of ast_node
+	std::vector<ast_node *> children;
+};
+
+// syntax tree
+struct ast_body{
+	const char *head_symbol = "<body>";
+	/*
+	std::string head_symbol;
+	*/
+	std::vector<ast_node*> children;
+};
+
+// stack to maintain leafs and nodes for AST
+std::deque<ast_node*> S_ast;
+
+// root element for syntax tree v1
+ast_node *ast_root = new ast_node;
+// c-root is not used anywhere right now
+ast_node *c_root = ast_root;
+
+// root element for syntax tree v2
+ast_body *syn_root = new ast_body;
+
+// make a node for AST
+ast_node *make_ast_node(std::string op, struct ast_node *left_leaf, struct ast_node *right_leaf);
+
+// make a leaf for AST
+ast_node *make_ast_leaf(std::string val);
+
+// Prints the branch in pre-order like fashion
+void LevelOrderTraversalEACHROOT(ast_node *root);
+
+// create a branches on node
+ast_node *create_branch(ast_node *i_root, ast_node *branch);
+
+// central node creation of exp evaluation
+ast_node *central_node_creation(std::string op);
+
+// alternate central node creation for exp evaluation
+ast_node *central_node_creation_exp(std::string op);
+
+// declaration and expression
+ast_node *central_node_creation_declaration(std::string op);
+
+// brnachwise level order traversal of tree
+void LevelOrderTraversalAST();
+
+// print all deue elements
+void print_stack_elements();
+
+// deprecated IF branch
+void IF_node_create_branch();
+
+// declare and assigned branch
+void declare_and_assign_branch();
+
+// declare and assign node
+void declare_assign_node_creation();
+
+// popping pattern in loops 
+ast_node *general_declaration_in_loop();
+
+// if branch execution
+void IF_Alternate();
